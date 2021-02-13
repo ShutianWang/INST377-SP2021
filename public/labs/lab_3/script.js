c596ed58cb1a6e8f4067ab73d9519e573e9b0d65
ShutianWang <77990268+ShutianWang@users.noreply.github.com>
2021-02-12 21:18:00 -0600

    lab3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,26 @@
-/* Put your javascript in here */
+ /* label the images, just for convenience, to visually track them */
+ function arrayMethodDemo() {
+    const array1 = [1, 2, "3", 4, 5];
+    
+      const listContainer = document.createElement('ul');
+      const target = document.querySelector('#box1');
+    target.append(listContainer);
+    
+      const array2 = array1.map(element => {
+      const listItem = document.createElement('li');
+      listItem.innerText = element;
+      listContainer.append(listItem);
+      return typeof element;
+    })
+    
+    
+    console.log(array2);
+  }
+  
+  function onLoadOfPage() {
+      document.addEventListener('click', (event) => {
+        arrayMethodDemo()
+    })
+  }
+  
+  window.onload = onLoadOfPage;
